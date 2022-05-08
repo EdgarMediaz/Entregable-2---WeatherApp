d00d3b62c5b4fd512f4fd8d166c8cba61eeba25e
Edgar Medina <edgar.mediaz@gmail.com>
2022-05-08 09:59:38 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [weather, setWeather] = useState({})
+  const [isCelcious, setIsCelcious] = useState(true);
+
+  useEffect(() => {
+    function success(pos) {
+      var crd = pos.coords;
+    
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=234e9b3fa7ea3eb74f854cbf3d893230`)
+        .then(res => setWeather(res.data))
+    }
+    
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, [])
+
+  console.log(weather);
+
+  const changeTemperature = () => setIsCelcious(!isCelcious)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="card">
+        <h1>Weather App</h1>
+        <h2>
+          {weather.name}, {weather.sys?.country} 
+        </h2>
+        <img src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>Temperature: </b> {isCelcious ? `${(weather.main?.temp - 273.15).toFixed(2)} °C` : `${((weather.main?.temp - 273.15) * 9/5 + 32).toFixed(2)} °F`}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <b>Wind: </b>{weather.wind?.speed} m/s
+        </p>
+        <button onClick={changeTemperature}>Degrees °C/°F</button>
+      </div>
     </div>
   );
 }
